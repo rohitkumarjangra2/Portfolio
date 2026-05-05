61248dbb4f41bfbe91635925e6a3d631883e6b6d
Rohit Kumar <rohit2004jangra@gmail.com>
2026-05-05 21:48:13 +0530

    adding photo

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -129,7 +129,7 @@ export const Hero = () => {
               />
               <div className="relative glass rounded-3xl p-2 glow-border">
                 <img
-                  src="/profile-photo.jpg"
+                  src="/profile_photo.jpg"
                   alt="Rohit Kumar"
                   className="w-full aspect-[4/5] object-cover rounded-2xl"
                 />
